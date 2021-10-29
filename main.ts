56f287c541124388752b3350082f7c42e5645364
macekviktor21 <93374573+macekviktor21@users.noreply.github.com>
2021-10-29 10:44:41 +0200

    přidán zvuk.

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    music.setVolume(255)
+    for (let index = 0; index < 4; index++) {
+        music.playMelody("A - C5 C G - E - ", 500)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    music.setVolume(255)
+    for (let index = 0; index < 4; index++) {
+        music.playMelody("- B C5 E G - C B ", 500)
+    }
+})
 input.onGesture(Gesture.Shake, function () {
     basic.showNumber(randint(1, 6))
     basic.pause(1000)
